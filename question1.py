aa9aa924744279e85f5c837865de6fccb17231b9
Sahil <yadavsahil1703@gmail.com>
2024-11-07 22:04:46 +0530

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,5 @@
+principle = int(input("enter principle amount "))
+rate = int(input("enter rate amount "))
+time = int(input("enter time "))
+si = (principle*rate*time)/100
+print("simple intrest is" , si)
