a510f7d676fc408d42cf73f72618ac03184db0ca
Sahil <yadavsahil1703@gmail.com>
2024-11-07 21:50:34 +0530

    Create heads and tails.py

---
+++ heads and tails.py
@@ -0,0 +1,12 @@
+#heads and tails using if and else statements.
+# User input head or tail
+# head : win
+# tail : loose
+
+print("choose head or tail")
+choice = input("Enter choice")
+
+if(choice == "head"):
+    print("U won")
+if(choice == "tail"):
+    print("U lose")
