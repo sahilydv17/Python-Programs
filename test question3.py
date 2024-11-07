b79d06e908c189d09b3d8888fee18ff44715299d
Sahil <yadavsahil1703@gmail.com>
2024-11-07 22:33:23 +0530

    Create test question3.py

---
+++ test question3.py
@@ -0,0 +1,7 @@
+#write a proogram to print square of a number if number is even else print num * 2.
+
+for i in range(1,20,1):
+    if (i%2==0):
+        print(i,"*",i ,"=" ,i*i)
+    else:
+        print(i,"*" ,2, "=" ,i*2)
