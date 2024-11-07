bc20a9e0dbf6062fd064779393358dd80d176ba8
Sahil <yadavsahil1703@gmail.com>
2024-11-07 21:47:10 +0530

    Create divisibilty of a number.py

---
+++ divisibilty of a number.py
@@ -0,0 +1,14 @@
+num = int(input("Enter number"))
+
+# print("Number is ",num)
+# u want to print this num only when num is div by 2
+
+
+# % always give u remainder
+# = means u are assigning some value
+# num1 = 10
+# == means u are comparing two values
+
+# condition is : num is even (div by 2 / rem ==0)
+if num % 2 == 0:
+    print("Number is ",num)
