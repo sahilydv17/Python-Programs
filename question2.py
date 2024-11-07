78902f5e2a44c8e8ce715cb19dd09871d7f80b60
Sahil <yadavsahil1703@gmail.com>
2024-11-07 22:05:16 +0530

    Create question2.py

---
+++ question2.py
@@ -0,0 +1,7 @@
+#area and perimeter of rectangle
+length = float(input("enter length "))
+breadth = float(input("enter breadth "))
+area = (length*breadth)
+perimeter = (length+breadth)*2
+print("area of reactangle is " , area)
+print("perimeter of rectangle is " , perimeter)
