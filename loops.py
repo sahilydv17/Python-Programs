666d47cb4567aacf2530297fb95a6ea407598db3
Sahil <yadavsahil1703@gmail.com>
2024-11-07 21:58:46 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,24 @@
+#for i in range(5):
+    #print(i)
+#for i in range(5):
+   # print("hello")
+#i is variable.
+#for is key word.
+#range is used to define repetition and always start with 0.
+# will end with (range value - 1).
+#for i in range(5):
+   # print(i, ".hello")
+#behind the scenes
+#range(5):range is (0,5,1).
+#0 is the starting value
+#5 is ending value in this
+#1 is the increment value
+#for i in range(0,10,2):
+    #print(i)
+#for i in range(3,20,4):
+ #   print(i)
+#for i in range(0,-5,-1):
+ #   print(i)
+#for i in range(2,22,2):
+#    print(i)
+
